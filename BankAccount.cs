f08b804c2eedd7c77c04af807baaa9411f26b93f
Johnathon Crawford <47326832+Kodi125@users.noreply.github.com>
2020-04-13 21:00:17 +0100

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,34 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date :
+ * Description : Lab Nine 2020
+ * 
+ * ############################# */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNine
+{
+    public class BankAccount
+    {
+        public decimal Balance { get; set; }
+        public decimal OverdraftLimit { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(decimal amount)
+        {
+            decimal availableFunds = Balance + OverdraftLimit;
+            if (amount <= availableFunds)//ensuring there is enough money
+            {
+                Balance -= amount;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/* #############################
+ * 
+ * Author: Johnathon Mc Grory
+ * Date :
+ * Description : Lab Nine 2020
+ * 
+ * ############################# */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNine
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
